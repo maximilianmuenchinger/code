da3a51b12ac703528bd088316444d936bb261e6b
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2021-11-08 23:23:48 +0100

    Add files via upload

--- tunierscript.js
+++ tunierscript.js
@@ -145,8 +145,8 @@ document.getElementById("auslosungErgebnis").appendChild(auslosung);
 //Datenbank und Server
 async function submitVerarbeiten1() {
     let formData = new FormData(document.forms[0]);
-    let string = localStorage.getItem("storage1");
-    let stringFormData1 = formData.append("Bestellung:", string);
+    let string = "bestellung:";
+    let stringFormData1 = formData.append("Bestellung", string);
     let url = "https://pongalong1.herokuapp.com/senden";
     let query = new URLSearchParams(formData);
     let query2 = new URLSearchParams(stringFormData1);
