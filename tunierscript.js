8c12252d33102d674f932d72bc6f01c5685ab34e
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2021-11-09 11:40:54 +0100

    Add files via upload

--- tunierscript.js
+++ tunierscript.js
@@ -152,7 +152,7 @@ async function submitVerarbeiten1() {
     let query2 = new URLSearchParams(stringFormData1);
     url += "?" + query.toString() + query2;
     let response = await fetch(url);
-    let answer = response.text();
+    let answer = response.toString();
     //let split = answer.split("},");
 console.log(answer);
     
