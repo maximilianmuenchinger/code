cc1d0421f3e50e5aefed32af9817b969f5034f68
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2021-11-09 12:00:54 +0100

    Add files via upload

--- tunierscript.js
+++ tunierscript.js
@@ -154,6 +154,8 @@ async function submitVerarbeiten1() {
     let response = await fetch(url);
     let responseUrl = response.url;
     let responseString = responseUrl.replace("https://pongalong1.herokuapp.com/senden?Bestellung=", "");
+    let responseString2 = responseString.replace("%2C", "");
+    let responseString3 = responseString2.replace("+", " ");
 
     console.log(responseString);
 
