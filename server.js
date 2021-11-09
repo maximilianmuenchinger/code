8ca2fb70dda65186157a486b7517b8df897a5f47
maximilianmuenchinger <64194143+maximilianmuenchinger@users.noreply.github.com>
2021-11-09 10:38:17 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -40,16 +40,10 @@ var Endabgabe;
 
 
         //Test
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
+       
             var dbo = db.db("mydb");
             var myobj = { name: "Company Inc", address: "Highway 37" };
-            dbo.collection("customers").insertOne(myobj, function(err, res) {
-              if (err) throw err;
-              console.log("1 document inserted");
-              db.close();
-            });
-          });
+            dbo.collection("Test1").insertOne(myobj);
           //testende
 
 
